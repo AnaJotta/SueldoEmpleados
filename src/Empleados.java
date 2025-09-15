8511b3a9bfcd674b5b55d81c39400e4bfa178e0d
AnaJotta <anajessicamarinmorales@gmail.com>
2025-09-15 21:55:12 +0200

    subida de proyecto terminado

--- src/Empleados.java
+++ src/Empleados.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
 
-public class Empleados {
+public abstract class Empleados {
+    protected double sueldoBase;
+
+    public Empleados(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public abstract double calcularSueldo();
+}
+
+class Repositor extends Empleados {
+    public Repositor() {
+        super(15890);
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return sueldoBase * 1.10;
+    }
+}
+class Cajero extends Empleados {
+    public Cajero() {
+        super(25630.89);
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return sueldoBase;
+    }
+}
+
+class Supervisor extends Empleados {
+    public Supervisor() {
+        super(35560.20);
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return sueldoBase * 0.89;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String tipoEmpleados = JOptionPane.showInputDialog("Introduzca el tipo de empleado (repositor / cajero / supervisor):");
+
+        Empleados empleado;
+
+        if (tipoEmpleados.equalsIgnoreCase("repositor")) {
+            empleado = new Repositor();
+        } else if (tipoEmpleados.equalsIgnoreCase("cajero")) {
+            empleado = new Cajero();
+        } else if (tipoEmpleados.equalsIgnoreCase("supervisor")) {
+            empleado = new Supervisor();
+        } else {
+            JOptionPane.showMessageDialog(null, "El tipo de empleado no es válido.");
+            return;
         }
+
+        double sueldoFinal = empleado.calcularSueldo();
+
+        JOptionPane.showMessageDialog(null, "El sueldo del " + tipoEmpleados + " es: " + sueldoFinal);
     }
 }
